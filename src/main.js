6083e67aaffd894f896dd5b0219eeaaff19a82ae
IN UH LEE <inuhlee@INui-MacBookPro.local>
2020-03-07 15:23:20 +0900

    api주소 변경

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router/index.js';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
 
-import axios from 'axios';
+import axios from "axios";
 
-const DEPLOY_IP = 'https://api.mask-nearby.com';
-const LOCAL_IP = 'http://localhost:3000';
+// const DEPLOY_IP = 'https://api.mask-nearby.com';
+const DEPLOY_IP = "http://mask-api.com";
+const LOCAL_IP = "http://localhost:3000";
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 axios.defaults.baseURL = DEPLOY_IP;
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	render: h => h(App),
-}).$mount('#app');
+  router,
+  render: h => h(App)
+}).$mount("#app");
