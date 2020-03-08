73315458773d5fa27a0170f06f00b1f36f8d2c29
ChoiJuwon <choijuwon@ChoiJuwons-MacBook-Pro.local>
2020-03-08 14:00:23 +0900

    before pull

--- src/router/router.js
+++ src/router/router.js
@@ -1,30 +1,30 @@
-import Main from '../components/Main.vue';
-import KakaoMap from '../components/KakaoMap.vue';
+import Main from "../components/Main.vue";
+import KakaoMap from "../components/KakaoMap.vue";
 
 const routes = [
-	{
-		path: '/',
-		name: 'Main',
-		component: Main,
-	},
-	{
-		path: '/map',
-		name: 'KakaoMap',
-		component: KakaoMap,
-		beforeEnter: (to, from, next) => {
-			// console.log(new Date().getHours());
+  {
+    path: "/",
+    name: "Main",
+    component: Main
+  },
+  {
+    path: "/map",
+    name: "KakaoMap",
+    component: KakaoMap
+    // beforeEnter: (to, from, next) => {
+    // 	// console.log(new Date().getHours());
 
-			to.params.maskData ? next() : next('/');
-			// if (!to.params.maskData) next('/');
-			// else next();
-		},
-	},
-	{
-		path: '*',
-		beforeEnter: (to, from, next) => {
-			next('/');
-		},
-	},
+    // 	to.params.maskData ? next() : next('/');
+    // 	// if (!to.params.maskData) next('/');
+    // 	// else next();
+    // },
+  },
+  {
+    path: "*",
+    beforeEnter: (to, from, next) => {
+      next("/");
+    }
+  }
 ];
 
 export default routes;
